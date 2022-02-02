4ceebf5f2243ce31fcfd25671ff6b6d1bf29c4b0
Denzel <dbartolaba123@gmail.com>
2022-02-01 16:10:51 -0800

    Finished Main dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 
+import { useState } from 'react';
 import './App.css';
+import OrderImages from './OrderImages';
+import MainDropdown from './Dropdowns/MainDropdown';
+
 
 function App() {
+  const [mainId, setMainId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [orderName, setOrderName] = useState('Charles');
+  const [instructions, setInstructions] = useState(['Please make curry extra spicey!!']);
   return (
     <div className="App">
+      <OrderImages mainId={mainId} sideId={sideId} drinkId={drinkId} />
+
+      <h1>Order For: {orderName}</h1>
+      <div className='bottom'>
+
+        <section className='dropdown'>
+          <MainDropdown setMainId={setMainId} />
+        </section>
+
+      </div>
      
     </div>
   );
--- src/OrderImages.js
+++ src/OrderImages.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function OrderImages({ mainId, sideId, drinkId }) {
   return <div className='food'>
-    <img className='main-img' src={`main-${mainId}.jpeg`} />
-    <img className='side-img' src={`side-${sideId}.jpeg`} />
-    <img className='drink-img' src={`drink-${drinkId}.jpeg`} />
+    <img className='main-img' src={`main-${mainId}.jpg`} />
+    <img className='side-img' src={`side-${sideId}.jpg`} />
+    <img className='drink-img' src={`drink-${drinkId}.jpg`} />
   </div>;
 }
